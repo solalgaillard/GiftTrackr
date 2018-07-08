78d253e784ec7a496f014b0a31355f1936db5f67
Solal Gaillard <solalgaillard@gmail.com>
2018-07-08 12:39:33 -0400

    Update NavBar.js

--- react/src/components/navBar/NavBar.js
+++ react/src/components/navBar/NavBar.js
@@ -4,7 +4,7 @@ import cx from "classnames";
 
 const NavBar = (props) =>
     <div className={cx('nav','row', 'center-xs')}>
-        <img className={cx('back')} onClick={window.history.back()} src='https://image.flaticon.com/icons/png/512/54/54906.png'/>
+        <img className={cx('back')} src='https://image.flaticon.com/icons/png/512/54/54906.png'/>
         <div className={cx('title')}>GiftTrackr</div>
         <img className={cx('cart')} src="https://www.freeiconspng.com/uploads/cart-icon-16.png" />
     </div>
